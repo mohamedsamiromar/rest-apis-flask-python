64e3a381df020312aa475a2fbd677913ee02ba2a
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-09-25 22:48:23 +0200

    run project on Docker

--- app.py
+++ app.py
@@ -1,12 +1,85 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
+data = {
+    "count": 3,
+    "next": "null",
+    "previous": "null",
+    "results": [
+        {
+            "id": 18,
+            "date": "2022/03/16",
+            "day": "Wednesday",
+            "corporate_name": "Mohamed Samir",
+            "city": "Medinah",
+            "store": "Headquarter",
+            "munjiz": {
+                "full_name": "Mohamed Omar Samir",
+                "mobile_number": "0501006444",
+                "nationality": "Saudi Arabia",
+                "bank_name": "null",
+                "iban": "null"
+            },
+            "actual_hours": 10,
+            "js_hrs_price": 25,
+            "munjiz_commission": 5,
+            "total_js_salary": 250,
+            "total_munjiz_commission": 50,
+            "amount": 300,
+            "paid": "True"
+        },
+        {
+            "id": 19,
+            "date": "2022/03/16",
+            "day": "Wednesday",
+            "corporate_name": "Mohamed Samir",
+            "city": "Medinah",
+            "store": "Headquarter",
+            "munjiz": {
+                "full_name": "Mohamed Omar Samir",
+                "mobile_number": "0501006444",
+                "nationality": "Saudi Arabia",
+                "bank_name": "null",
+                "iban": "null"
+            },
+            "actual_hours": 10,
+            "js_hrs_price": 25,
+            "munjiz_commission": 5,
+            "total_js_salary": 250,
+            "total_munjiz_commission": 50,
+            "amount": 300,
+            "paid": "True"
+        },
+        {
+            "id": 17,
+            "date": "2022/04/30",
+            "day": "Saturday",
+            "corporate_name": "Mohamed Samir",
+            "city": "Medinah",
+            "store": "Headquarter",
+            "munjiz": {
+                "full_name": "Ali Mohamed Ali",
+                "mobile_number": "0508741036",
+                "nationality": "Egypt",
+                "bank_name": "null",
+                "iban": "null"
+            },
+            "actual_hours": 10,
+            "js_hrs_price": 25,
+            "munjiz_commission": 5,
+            "total_js_salary": 250,
+            "total_munjiz_commission": 50,
+            "amount": 300,
+            "paid": "True"
+        }
+    ]
+}
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello, There is a First Flask APP'
-
+@app.get("/store")
+def get_store():
+    return {"store": data}
 
 if __name__ == '__main__':
     app.run()
